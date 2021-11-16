97c894bc6709aeebdca4db82e2386b261b46b344
williamd5 <will.duncan.nn+github@gmail.com>
2021-11-16 13:04:54 +0200

    add jsdoc

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-class AlgorithmBibliotheca {
-    // perform async task n times
+
+class AlgorithmBibliotheca {s
+    /**
+     * Perform asynchronous task n times (awaits execution before continuing iteration)
+     * @param  {Number} n Number of iterations to perform
+     * @param  {Function} fn Task to run each iteration; supplied argument is k of current iteration (countdown)
+     * @param  {Collection} collector Collection preset
+     * @return {Promise} Resolves into {Collection} containing return values from the tasks in their corresponding order
+     */
     static async asyncTimes (n = 0, fn = new Function, collector = new AlgorithmBibliotheca.Collection) {
         if (n <= 0) return collector;
         return new Promise((resolve, reject) => {
@@ -12,14 +19,27 @@ class AlgorithmBibliotheca {
         })
     }
 
-    // perform sync task n times
+    /**
+     * Perform synchronous task n times
+     * @param  {Number} n Number of iterations to perform
+     * @param  {Function} fn Task to run each iteration; supplied argument is k of current iteration (countdown)
+     * @param  {Collection} collector Collection preset
+     * @return {Collection} Contains return values from the tasks in their corresponding order
+     */
     static syncTimes (n = 0, fn = new Function, collector = new AlgorithmBibliotheca.Collection) {
         if (n <= 0) return collector;
         while(n > 0) collector.push(fn(n--));
         return collector;
     }
 
-    // determine whether task is sync/async
+    /**
+     * Determine type of iteration to perform based on the result from the first task.
+     * If the result is {Promise}, `.asyncTimes()` is performed. Otherwise, `.syncTimes()` is performed.
+     * @param  {Number} n Number of iterations to perform
+     * @param  {Function} fn Task to run each iteration; supplied argument is n of current iteration (countdown)
+     * @param  {Collection} collector Collection preset
+     * @return {Promise|Collection} Resolves into or returns {Collection} containing return values from the tasks in their corresponding order
+     */
     static times (n = 0, fn = new Function, collector = new AlgorithmBibliotheca.Collection) {
         if (n <= 0) return collector;
         if (typeof fn !== "function") {
@@ -73,6 +93,7 @@ class AlgorithmBibliotheca {
 // define alias
 const AB = AlgorithmBibliotheca;
 
+// aliases for .times()
 function once () {
     return AlgorithmBibliotheca.times(1, ...arguments);
 };
@@ -84,6 +105,7 @@ function thrice () {
 };
 
 
+// prototypes
 Number.prototype.asyncTimes = function () {
     return AlgorithmBibliotheca.asyncTimes(this, ...arguments);
 }
